1dd115ece02ea6afdbba008c86b4abad6185c0d9
Sumit Prajapati <prajapati.3@iitj.ac.in>
2022-04-30 12:20:41 +0530

    done fetching data from backend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 # model = pickle.load(open('model.pkl', 'rb'))
@@ -9,15 +10,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+def process(form_data):
+    data = pd.Series(form_data)
+    continuous_column = ['Age','RestingBP','Cholesterol','FastingBS','MaxHR','Oldpeak']
+    for index,value in data.iteritems():
+        if index in continuous_column:
+            if len(value)==0:
+                value = 0
+            data[index] = int(value)
+    df = pd.DataFrame(data=[data.values],columns=data.index)
+
+    return df
+
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    print(request.form.values())
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+  
+    df = process(request.form)
+    
+    return render_template('predict.html', percent = 89.6)
 
 
 if __name__ == "__main__":
