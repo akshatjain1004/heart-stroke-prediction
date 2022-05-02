ef01d848651eadf42be5cc50d722c386910f7cfe
Sumit Prajapati <prajapati.3@iitj.ac.in>
2022-05-02 19:20:48 +0530

    Working model with MLP Classifier

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def process(form_data):
         if index in continuous_column:
             if len(value)==0:
                 value = 0
-            data[index] = int(value)
+            data[index] = float(value)
     df = pd.DataFrame(data=[data.values],columns=data.index)
 
     return df
@@ -27,7 +27,6 @@ def predict():
   
     df = process(request.form)
     prediction = model.predict_proba(df)[0]
-    # prediction= 86.5
     return render_template('predict.html', prediction = round(prediction[1]*100,2))
 
 
--- model.py
+++ model.py
@@ -6,40 +6,35 @@ import pandas as pd
 import numpy as np
 import pickle
 import warnings
-warnings.filterwarnings('ignore')
-
-df = pd.read_csv('heart.csv')
-
-
-X = df.drop('HeartDisease',axis=1)
-y = df['HeartDisease']
-
-
-categorical_cols = [col for col in X.columns if X[col].nunique() < 10 and X[col].dtype == "object"]
-
-numerical_cols = [col for col in X.columns if X[col].dtype in ['int64', 'float64']]
-
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.neural_network import MLPClassifier
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('heart.csv')
+clf = MLPClassifier(solver= 'lbfgs', random_state= 6, max_iter= 1100, hidden_layer_sizes= 10, alpha= 1e-06)
+
+# selected features
+selected_features= ['Age', 'Sex', 'ChestPainType', 'FastingBS', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
+
+X = df[selected_features]
+y = df['HeartDisease']
+
+categorical_cols = [col for col in X.columns if X[col].nunique() < 10 and X[col].dtype == 'object']
+
+numerical_cols = [col for col in X.columns if X[col].dtype in ['int64', 'float64']]
 
 preprocessor = ColumnTransformer(
     transformers=[
         ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols),
         ('num', StandardScaler(), numerical_cols)
     ])
-
-
-from sklearn.neural_network import MLPClassifier
-from xgboost import  XGBClassifier
-
-clf = XGBClassifier(use_label_encoder=False)
-
-
 my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                               ('classifier', clf)
                              ])
+
 
 my_pipeline.fit(X, y)
 
